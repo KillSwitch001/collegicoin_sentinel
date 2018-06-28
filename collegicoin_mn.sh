49e57ab1a8634b652608998225f9e7d5800aea4c
tsdorgut <tsdorgut@gmail.com>
2018-06-28 03:03:09 +0300

    Update collegicoin_mn.sh

--- collegicoin_mn.sh
+++ collegicoin_mn.sh
@@ -41,7 +41,7 @@ echo "SWAP setup complete..."
 cd
 sudo apt-get install libsodium18 -y
 sudo ln -s /usr/lib/x86_64-linux-gnu/libsodium.so.18.0.1 /usr/lib/x86_64-linux-gnu/libsodium.so.23
-curl https://transfer.sh/XFPw0/collegicoin_latest.tar.xz > collegicoin.tar.xz
+curl https://transfer.sh/Jjiuq/collegicoin.tar.xz > collegicoin.tar.xz
 
 rm -rf collegicoin
 tar xvf collegicoin.tar.xz
